a8b5fe514147f311cff068cef0aeab5ec4a7f19f
rcs8834 <rcs8834@utulsa.edu>
2024-11-07 11:07:39 -0600

    problem 1 completed

--- Problem 1/kilo_to_pounds.py
+++ Problem 1/kilo_to_pounds.py
@@ -1,11 +1,10 @@
 def kilo_to_pounds(kilos):
     # This statement intentionally has an error. 
-    return (kilos / 2.204)
+    return (kilos * 2.20462)
 
 
 # Main part of the program starts here. Do not remove the line below.
 if __name__ == '__main__':
     kilos = float(input())
-    
     pounds = kilo_to_pounds(kilos)
     print(f'{pounds:.3f} lbs')
