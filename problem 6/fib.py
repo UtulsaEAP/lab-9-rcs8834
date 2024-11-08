d313b895b649cd57e943060747f24e4fd65e2ca2
rcs8834 <rcs8834@utulsa.edu>
2024-11-08 17:33:14 -0600

    all problems completed

--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,7 +1,21 @@
 def fibonacci(n):
     
     #write your code here
-    return
+    if n < 0:
+        return -1
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        x = 0
+        y = 1
+        while n > 1:
+            new_num = x+y
+            x = y
+            y = new_num
+            n -= 1
+    return new_num
 
 if __name__ == '__main__':
     start_num = int(input())
